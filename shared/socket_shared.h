8be491b0ff302b6bfda5b104c0cd05f3d5bf855c
Dirsnik <rondiskin@mail.tau.ac.il>
2020-01-11 12:49:25 +0200

    Add shared files + directory

---
+++ shared/socket_shared.h
@@ -0,0 +1,9 @@
+#ifndef SOCKET_SHARED
+#define SOCKET_SHARED
+
+#define SERVER_ADDRESS_STR "127.0.0.1"
+#define SERVER_PORT 2345
+
+#define STRINGS_ARE_EQUAL( Str1, Str2 ) ( strcmp( (Str1), (Str2) ) == 0 )
+
+#endif // SOCKET_SHARED
